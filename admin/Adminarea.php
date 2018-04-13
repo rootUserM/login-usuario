b6a4e6f3be8e30fec7e0ae7f52eb5cc0a0470797
Marlene-tapia <37878975+Marlene-tapia@users.noreply.github.com>
2018-04-12 21:27:39 -0500

    Update Adminarea.php

--- admin/Adminarea.php
+++ admin/Adminarea.php
@@ -15,43 +15,32 @@ include_once 'templates/header.php';
         Blank page
         <small>it all starts here</small>
       </h1>
-      <?php  /*<ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li> 
-      </ol>*/?>
+      <?php  ?>
     </section>
 
-    <!-- Main content -->
+   
     <section class="content">
 
-      <!-- Default box -->
+     
       <div class="box">
         <div class="box-header with-border">
           <h3 class="box-title">Title</h3>
 
-       <?php  /*  <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button> 
-          </div>*/?>
+       <?php ?>
         </div>
         <div class="box-body">
           Start creating your amazing application!
         </div>
-        <!-- /.box-body -->
+        
         <div class="box-footer">
           Footer
         </div>
-        <!-- /.box-footer-->
+        
       </div>
-      <!-- /.box -->
+      
  
     </section>
-    <!-- /.content -->
+   
   </div>
-  <!-- /.content-wrapper -->
-
+  
   <?php include_once 'templates/footer.php';?>
